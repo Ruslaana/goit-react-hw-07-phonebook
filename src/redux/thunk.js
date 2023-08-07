92fd51e557786e2c14801b8939172b71e7ff8f9b
Ruslaana Onyshchuk <lalelya7@gmail.com>
2023-08-07 14:46:37 +0200

    Update thunk.js

--- src/redux/thunk.js
+++ src/redux/thunk.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://64ce23970c01d81da3ee8ad9.mockapi.io';
+axios.defaults.baseURL = 'https://64ce23970c01d81da3ee8ad9.mockapi.io/contacts/';
 
 export const getAllContacts = createAsyncThunk(
   'contacts/getAllContacts',
