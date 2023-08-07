55aaf5754a6f56a0a8f65435e2f41c24c31d3e1c
Ruslaana Onyshchuk <lalelya7@gmail.com>
2023-08-07 21:35:53 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Section from './Section';
 import ContactForm from './ContactForm';
 import FilterInput from './FilterInput/FilterInput';
 import ContactsList from './ContactsList/ContactsList';
+import { useDispatch } from 'react-redux';
+import { getAllContacts } from 'redux/thunk';
 
 const App = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+    dispatch(getAllContacts());
+  }, [dispatch]);
 
   return (
     <>
